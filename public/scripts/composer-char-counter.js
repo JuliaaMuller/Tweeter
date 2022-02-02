1e4ba36e754d2a4c0a406fbf2aa928771f764af2
JuliaaMuller <julialm.muller@gmail.com>
2022-02-01 20:29:43 -0500

    tweeter app with css and old tweet box

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $("#tweet-text").on("input",function(event) {
+    let $textInput = $(this);
+    let $textLength = $textInput.val().length;
+    let $counterSelected = $textInput.closest("form").find(".counter")
+    let $counterValue = 140 - $textLength;
+
+    $counterSelected.text($counterValue);
+
+    if ($counterValue < 0) {
+      $(".counter").css("color", "red");
+    } else {
+      $(".counter").css("color", "#425252");
+    }
+  });
+});
+
+
